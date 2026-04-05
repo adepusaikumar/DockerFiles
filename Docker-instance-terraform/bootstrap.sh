3a90bde5561a79d07a4f084c7cc8c96e030aae1e
saikumar001 <adepusaikumar0076@gmail.com>
2026-04-05 14:04:46 +0530

    docker

---
+++ Docker-instance-terraform/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+growpart /dev/nvme0n1 4
+lvextend -L +30G /dev/RootVG/varVol
+xfs_growfs /var
+
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl enable --now docker
+docker run hello-world
+
---
+++ Docker-instance-terraform/main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "docker_instance" {
+  ami           = data.aws_ami.joindevops.id
+  instance_type = "t3.micro"
+  user_data = file("./bootstrap.sh")
+
+  tags = {
+    Name = "DockerInstance"
+  }
+  
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+    delete_on_termination = true
+  }
+  vpc_security_group_ids = ["sg-05f6d4e8dbe538e8e"]
+}
